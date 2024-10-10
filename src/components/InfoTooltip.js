d55199de3e900d593bc740e9858f8c2fde542459
Abai369 <jcaleb1110@gmail.com>
2024-10-09 18:11:21 -0700

    <feat><css styles>

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import '../blocks/infoTooltip.css';
 
 function infoTooltip({ isOpen, onClose, message}) {
     return (
-        isOpen && (
-            <div className="modal">
-                <div className="modal-content">
-                    <span className="close" onClick={onClose}>&times;</span>
-                    <p>{message}</p>
+            <div className={`tooltip ${isOpen ? 'tooltip__open' : ''}`}>
+                <div className="tooltip__content">
+                    <p className="tooltip__message">{message}</p>
+                    <button href='Close__Icon.png' className="tooltip__close" onClick={onClose}>Close</button>
                 </div>
             </div>
-        )
-    );
+        );
 }
 
 export default infoTooltip;
