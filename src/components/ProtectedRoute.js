cb26b6a333defb97f7cd868883c15a0c50a874e0
Abai369 <jcaleb1110@gmail.com>
2024-10-11 17:57:37 -0700

    <fix><context Provider,main,current user>

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-
+import Main from './Main';
 function ProtectedRoute({ component: Component, isAuthenticated, ...rest }) {
         return (
         
             <>
             {
             isAuthenticated ? (
-                <Component {...rest} />
+                <Main {...rest} />
             ) : (
              <Navigate to  ={{
              pathname: "/signin",
